45b53382eaf2ead52fa90e185c724d9183c12f9e
svitass <52770259+svitass@users.noreply.github.com>
2021-08-31 21:24:15 +0800

    Delete reptile directory

--- reptile/request_post.py
+++
@@ -1,8 +0,0 @@
-import requests
-response_get = requests.get("http://httpbin.org/get")  # https://www.baidu.com/
-print("状态码：",response_get.status_code)
-print(response_get.text)
-
-response_post = requests.post("http://httpbin.org/post")  # 获得的信息相比于get()更多一点
-print("状态码：",response_post.status_code)
-print(response_post.text)
--- reptile/requests_put.py
+++
@@ -1,4 +0,0 @@
-import requests
-response = requests.put("http://httpbin.org/put")
-print(response.status_code)
-print(response.text)
